bf5ad75509eff0cee87ae0a83416932b8fb3bb3a
Pavanshub <krishna.dirishala05@gmail.com>
2024-10-23 18:07:02 +0530

    bg

--- src/components/admin-panel/footer.tsx
+++ src/components/admin-panel/footer.tsx
@@ -10,20 +10,20 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-white dark:bg-gray-900">
+    <footer>
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href="https://flowbite.com/" className="flex items-center">
               <PanelsTopLeft />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">
                 Makerble
               </span>
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold  uppercase ">
                 Organization
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
